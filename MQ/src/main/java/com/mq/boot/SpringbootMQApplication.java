75a57698a7dcb5987b184a5796b3716f9b93cf9a
chen <cheng_ch_cly@sina.com>
2022-05-30 07:26:12 +0800

    rabbitMQ

---
+++ MQ/src/main/java/com/mq/boot/SpringbootMQApplication.java
@@ -0,0 +1,12 @@
+package com.mq.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMQApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMQApplication.class,args);
+    }
+}
